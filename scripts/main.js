200fd1439c7fba70255dbcfed2d433b29c9f0dca
João Pedro Modesto <joaopedromodesto505@gmail.com>
2025-05-04 23:24:59 -0300

    Intro 'finalizada' e variaveisCSS(Cores e fontes)

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,30 @@
 import {type} from './type-writer.js';
 import {whatsapp_action} from './whatsapp.js';
 
-    //calling intro
-    const textTerminalIntro = [
-        '>Searching for professional...',
-        '>Professional found: "jpModesto.Dev"...',
-        '>Loading port folio of: "jpModesto.dev"...',
-        '>Connecting to creative interface...',
-        '>Hello, Modesto here — your future dev!'
-        ];
-    const typeWriter = document.getElementById("intro-writer");
-    const delay = 50;
-
+//calling intro
+const textTerminalIntro = [
+    '> Searching for professional...',
+    '> Professional found: "jpModesto.Dev"...',
+    '> Loading portfolio of: "jpModesto.dev"...',
+    '> Connecting to creative interface...',
+    '> - Hello, Modesto here — your future dev!'
+    ];
+const typeWriter = document.getElementById("intro-writer");
+const delay = 1;
+const introTerminal = document.getElementById("intro-terminal");
+setTimeout(() => {
     type(textTerminalIntro, typeWriter, delay, () =>{
-        const intro = document.getElementById("intro-terminal");
-        intro.style.display = "none";
+        introTerminal.style.opacity = "0";
+        introTerminal.addEventListener("transitionend", () => {
+            introTerminal.style.display = "none";
+            document.body.style.overflowY = "auto";
+        });
+    }, 1);
+}, 700);
 
-        const content = document.getElementById("content");
-        content.style.display = "block";
-    });
-
-    //Wheen click in WhatsApp icon:
-    const whatsappIcon = document.getElementById('whatsappIcon');
-    if(whatsappIcon)
-        whatsappIcon.addEventListener('click', whatsapp_action);
-    else 
-        console.error('WhatsApp icon not found');
+//When click in WhatsApp icon:
+const whatsappIcon = document.getElementById('whatsappIcon');
+if(whatsappIcon)
+    whatsappIcon.addEventListener('click', whatsapp_action);
+else 
+    console.error('WhatsApp icon not found');
--- scripts/type-writer.js
+++ scripts/type-writer.js
@@ -1,21 +1,25 @@
 let line = 0;
 let char = 0;
 
-export function type(text, docContent, typingDelay = 75, finished = null){
+export function type(text, docContent, typingDelay = 40, finished = null, linePause = 500){
     if(line < text.length){
         if(char < text[line].length){
-            docContent.textContent += text[line][char++];
-            setTimeout(() => type(text, docContent, typingDelay, finished), typingDelay);
+            docContent.innerHTML = docContent.innerHTML.replace('<span class="cursor">|</span>', '') + text[line][char++] + '<span class="cursor">|</span>';
+            setTimeout(() => type(text, docContent, typingDelay, finished, linePause), typingDelay);
         }
         else{
-            docContent.textContent += '\n';
-            line++;
-            char = 0;
-            setTimeout(() => type(text,docContent, typingDelay, finished), typingDelay);
+            setTimeout(() => {
+                if(line+1 < text.length)
+                    docContent.innerHTML = docContent.innerHTML.replace('<span class="cursor">|</span>', '') + '\n' + '<span class="cursor">|</span>';
+                line++;
+                char = 0;
+                type(text, docContent, typingDelay, finished, linePause);
+            }, linePause);
         }
     }
-    else if(finished){
-        finished();
+    else {
+        if(finished)
+            finished();
     }
 }
 
